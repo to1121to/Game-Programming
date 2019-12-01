4ef72b40587c92379225d276c6729b64d14d012d
To Hsu <to1121to@gmail.com>
2019-12-01 21:06:19 +0800

    12/1

---
+++ Assets/ReverseMaskComponent/Scripts/ReverseMaskLayer.cs
@@ -0,0 +1,38 @@
+﻿
+namespace UnityEngine.UI
+{
+    [AddComponentMenu("UI/Reverse Mask Layer")]
+    public class ReverseMaskLayer : Image
+    {
+        static protected Material s_DefaultReverseMaskBackgroundMaterial = null;
+
+        static public Material defaultReverseMaskLayerMaterial
+        {
+            get
+            {
+                if (s_DefaultReverseMaskBackgroundMaterial == null)
+                {
+                    s_DefaultReverseMaskBackgroundMaterial = ReverseMaskUtility.GetReverseMaskLayerMaterial();
+                }
+
+                return s_DefaultReverseMaskBackgroundMaterial;
+            }
+        }
+
+        /// <summary>
+        /// The specified Material used by this Image. The default Material is used instead if one wasn't specified.
+        /// </summary>
+        public override Material material
+        {
+            get
+            {
+                return defaultReverseMaskLayerMaterial;
+            }
+
+            set
+            {
+                base.material = value;
+            }
+        }
+    }
+}
---
+++ Assets/ReverseMaskComponent/Scripts/ReverseMaskUtility.cs
@@ -0,0 +1,19 @@
+﻿
+namespace UnityEngine.UI
+{
+    public static class ReverseMaskUtility
+    {
+        private const string REVERSE_MASK_SHADER_NAME = "UI/ReverseMask";
+        private const string REVERSE_MASK_LAYER_SHADER_NAME = "UI/ReverseMaskLayer";
+
+        public static Material GetReverseMaskMaterial()
+        {
+            return new Material(Shader.Find(REVERSE_MASK_SHADER_NAME));
+        }
+
+        public static Material GetReverseMaskLayerMaterial()
+        {
+            return new Material(Shader.Find(REVERSE_MASK_LAYER_SHADER_NAME));
+        }
+    }
+}
