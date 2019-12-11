de116a5d3d4391aac1bd26ac3dc271106f759b76
jameslg99 <jameslg99@users.noreply.github.com>
2019-12-11 08:21:10 +0800

    Add Dialogue

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject StartButton;
+    public GameObject DialogueBox;
+    public Text nameText;
+    public Text dialogueText;
+
+    //public Animator animator;
+    
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        DialogueBox.SetActive(false);
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        StartButton.SetActive(false);
+        DialogueBox.SetActive(true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        DialogueBox.SetActive(false);
+    }
+}
